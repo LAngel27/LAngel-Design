3393d8d4b074859b53038735951fa3a7fe5f0976
LAngel <luiscartaya27@hotmail.com>
2020-12-15 11:40:34 -0430

    agregando evento click al menu hamburguesa

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,31 @@
-const $ICON = document.getElementById('icon')
+
+const $NAVBAR = document.querySelector('.header-navbar')
+
+function toggle($element = $NAVBAR) {
+    $element.classList.toggle('header-navbar-active')
+}
+
+
+// function replace() {
+//     return(
+//         '<i class="fas fa-times header-container__hamburger"></i>'
+//     )
+// }
+
+// function html(fn) {
+//     let $document = document.implementation.createHTMLDocument()
+//     debugger
+//     $document.body.innerHTML = fn
+//     return $document.body.children[0]
+// }
+
+// html(replace)
+
+const $ICON = document.getElementById('menu-hamburger').addEventListener('click', () => {
+    toggle()
+})
+
+
 
 
 
