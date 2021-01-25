f60d0b859e84789884d49afdd7fc4959652768ca
luiscartaya27@hotmail.com <ainnara16>
2021-01-24 21:51:36 -0400

    corrigiendo errores

--- src/js/Index.js
+++ src/js/Index.js
@@ -1,29 +1,36 @@
-
 const $header = document.getElementById('init')
+const $main = document.querySelector("#hero")
 const $navbar = document.querySelector('.header-navbar')
 const $header__btn = document.querySelector('.header-button')
 const $title = document.querySelector("#services > h1")
 
+addEventListener('load', ev => {
+        $header.style.display = 'block'
+        $main.style.display = 'block'
+        $header.style.animation = 'reboot 2s forwards'
+        $main.style.animation = 'left 2s forwards'
+    
 
+})
 
-addEventListener('scroll', (ev) => {
+addEventListener('scroll', ev => {
     let index = ev.target.scrollingElement.scrollTop
+
 
     let div = index / 12
     let result = parseInt(div.toFixed(0))
     console.log(result)
 
     for (let i = result; i < 10000; i++) {
-        if( i > 1){
-            $header__btn.style.display = 'none'
-            $title.style.display = 'none'
-        } else {
+        if( i > 0){
+            
         }
     }
-})
+});
 
 
-$header__btn.addEventListener('click', () =>{
+
+$header__btn.addEventListener('click', () => {
     $header__btn.classList.toggle('header-button-active') 
     
 });
